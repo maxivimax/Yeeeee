a4facfb28093e15d7c24ccb197e9a16724c6ba32
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2021-01-20 17:03:30 +0300

    Updates

---
+++ precache-manifest.7f107c976d05448f3f5226d0a99bf224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475b095976b283e2faca45fe53cb6b1b",
+    "url": "/Yeeeee/index.html"
+  },
+  {
+    "revision": "023de33b8519a4cd968b",
+    "url": "/Yeeeee/static/css/main.7eb01f44.chunk.css"
+  },
+  {
+    "revision": "5e261f5bc206a105a386",
+    "url": "/Yeeeee/static/js/2.81a61c9d.chunk.js"
+  },
+  {
+    "revision": "023de33b8519a4cd968b",
+    "url": "/Yeeeee/static/js/main.96577ce2.chunk.js"
+  },
+  {
+    "revision": "ad6f6e40ec0d9eaa3030",
+    "url": "/Yeeeee/static/js/runtime~main.5bcaceab.js"
+  }
+]);
